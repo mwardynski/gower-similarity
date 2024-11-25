14215622b5bd09076391c3c3ee9dd7b6e369367d
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-11-25 07:57:34 +0100

    create .R file

---
+++ metrics_computation.R
@@ -0,0 +1,26 @@
+# clear memory and environment
+rm(list = ls(all.names = TRUE))
+
+# library imports
+library(readr)
+library(caret)
+
+adult <- read_csv("tests/tests_files/adult.csv")
+car_insurance_claim <- read_csv("tests/tests_files/car_insurance_claim.csv")
+diabetes <- read_csv("tests/tests_files/diabetes.csv")
+survey <- read_csv("tests/tests_files/survey.csv")
+
+# numerical variables normalization 0-1
+normalize <- function(file_csv){
+  my_numeric_columns <- sapply(file_csv, is.numeric)
+  
+  # check if there are any numeric columns to normalize
+  if (sum(numeric_cols) == 0) {
+    stop("Brak kolumn numerycznych do normalizacji w pliku: ", file_csv)
+  }
+  
+  preproc <- preProcess(data[, numeric_cols], method = c("range"))
+  normalized_data <- predict(preproc, data)
+  
+  return(normalized_data)
+}
