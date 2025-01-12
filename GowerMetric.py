[mix_var] correct calculation of gower for cat_ord

GowerMetric.py
@@ -106,39 +106,44 @@ def gower_metric_call_func(
             cat_ord_dist[cat_ord_calc_skip] = 0.0
         elif nan_values_handling == "max_dist":
             cat_ord_dist[cat_ord_calc_skip] = 1.0
-        
-        rank_col_1 = np.zeros(cat_ord_num)
-        rank_col_2 = np.zeros(cat_ord_num)
-        for i in range(cat_ord_num):
-            if cat_ord_calc_skip[i]:
-                continue
-            mapping = cat_ord_rank_mappings[i]
-            rank_col_1[i] = mapping[int(ordinal_cols_1[i])]
-            rank_col_2[i] = mapping[int(ordinal_cols_2[i])]
 
-        abs_ranks_dist = np.zeros(cat_ord_num)
-        abs_ranks_dist[~cat_ord_calc_skip] = np.abs(rank_col_1[~cat_ord_calc_skip] - rank_col_2[~cat_ord_calc_skip])
+        cat_ord_calc_skip_same = ordinal_cols_1 == ordinal_cols_2
+        cat_ord_dist[cat_ord_calc_skip_same] = 0.0
+        cat_ord_calc_skip |= cat_ord_calc_skip_same
         
-        first_ordinal_occur = np.zeros(cat_ord_num)
-        second_ordinal_occur = np.zeros(cat_ord_num)
-        max_ordinal_occur = np.zeros(cat_ord_num)
-        min_ordinal_occur = np.zeros(cat_ord_num)
-        for i in range(cat_ord_num):
-            if cat_ord_calc_skip[i]:
-                continue
-            first_ordinal_occur[i] = cat_ord_cardinalities[i][int(ordinal_cols_1[i])]
-            second_ordinal_occur[i] = cat_ord_cardinalities[i][int(ordinal_cols_2[i])]
-            max_ordinal_occur[i] = cat_ord_cardinalities[i][-1]
-            min_ordinal_occur[i] = cat_ord_cardinalities[i][0]
+        if np.any(~cat_ord_calc_skip):
+            rank_col_1 = np.zeros(cat_ord_num)
+            rank_col_2 = np.zeros(cat_ord_num)
+            for i in range(cat_ord_num):
+                if cat_ord_calc_skip[i]:
+                    continue
+                mapping = cat_ord_rank_mappings[i]
+                rank_col_1[i] = mapping[int(ordinal_cols_1[i])]
+                rank_col_2[i] = mapping[int(ordinal_cols_2[i])]
 
-        first_ordinal_occur[~cat_ord_calc_skip] = (first_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
-        second_ordinal_occur[~cat_ord_calc_skip] = (second_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
-        max_ordinal_occur[~cat_ord_calc_skip] = (max_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
-        min_ordinal_occur[~cat_ord_calc_skip] = (min_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
+            abs_ranks_dist = np.zeros(cat_ord_num)
+            abs_ranks_dist[~cat_ord_calc_skip] = np.abs(rank_col_1[~cat_ord_calc_skip] - rank_col_2[~cat_ord_calc_skip])
+            
+            first_ordinal_occur = np.zeros(cat_ord_num)
+            second_ordinal_occur = np.zeros(cat_ord_num)
+            max_ordinal_occur = np.zeros(cat_ord_num)
+            min_ordinal_occur = np.zeros(cat_ord_num)
+            for i in range(cat_ord_num):
+                if cat_ord_calc_skip[i]:
+                    continue
+                first_ordinal_occur[i] = cat_ord_cardinalities[i][int(ordinal_cols_1[i])]
+                second_ordinal_occur[i] = cat_ord_cardinalities[i][int(ordinal_cols_2[i])]
+                max_ordinal_occur[i] = cat_ord_cardinalities[i][-1]
+                min_ordinal_occur[i] = cat_ord_cardinalities[i][0]
 
-        cat_ord_dist[~cat_ord_calc_skip] = (abs_ranks_dist[~cat_ord_calc_skip] - first_ordinal_occur[~cat_ord_calc_skip] - second_ordinal_occur[~cat_ord_calc_skip]) / \
-                                (cat_ord_max_ranks_[~cat_ord_calc_skip] - cat_ord_min_ranks_[~cat_ord_calc_skip] - max_ordinal_occur[~cat_ord_calc_skip] - min_ordinal_occur[~cat_ord_calc_skip])
-        
+            first_ordinal_occur[~cat_ord_calc_skip] = (first_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
+            second_ordinal_occur[~cat_ord_calc_skip] = (second_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
+            max_ordinal_occur[~cat_ord_calc_skip] = (max_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
+            min_ordinal_occur[~cat_ord_calc_skip] = (min_ordinal_occur[~cat_ord_calc_skip] - 1) / 2
+
+            cat_ord_dist[~cat_ord_calc_skip] = (abs_ranks_dist[~cat_ord_calc_skip] - first_ordinal_occur[~cat_ord_calc_skip] - second_ordinal_occur[~cat_ord_calc_skip]) / \
+                                    (cat_ord_max_ranks_[~cat_ord_calc_skip] - cat_ord_min_ranks_[~cat_ord_calc_skip] - max_ordinal_occur[~cat_ord_calc_skip] - min_ordinal_occur[~cat_ord_calc_skip])
+            
         if weights is not None:
             cat_ord_dist = cat_ord_dist @ weights[ratio_scale_idx]
         else:
@@ -235,7 +240,7 @@ class MyGowerMetric:
         precomputed_weights_file: Optional[str] = None,
         nan_values_handling: str = "raise",
         number_of_clusters=None,
-        verbose: int = 0,
+        verbose: int = 0
     ):
         assert (
             weights is None

tests/UnitTestsGowerMetric.py
@@ -359,7 +359,7 @@ def test_podani_opt_cat_ord():
             ['low', 'M'],
             ['high', 'S'],
             ['medium', 'L'],
-            ['low', 'XL'],
+            ['low', 'L'],
             ['medium', 'XXL']
         ]
     )
@@ -372,7 +372,7 @@ def test_podani_opt_cat_ord():
     )
 
     enc = OrdinalEncoder(
-        categories="auto",
+        categories=[['low', 'medium', 'high'], ['S', 'M', 'L', 'XL', 'XXL']],
         dtype=np.float64,
         handle_unknown="use_encoded_value",
         unknown_value=np.nan,
@@ -384,10 +384,8 @@ def test_podani_opt_cat_ord():
 
     gower.fit(data)
     res = gower(data[0], data[1])
-    expected_result = 0.2917
-    tolerance = 0.0001
-    assert res-expected_result < tolerance
-
+    expected_result = 0.625
+    assert np.isclose(res, expected_result)
 
 def test_podani_opt_cat_ord_with_nans():
     data = pd.DataFrame(
@@ -409,7 +407,7 @@ def test_podani_opt_cat_ord_with_nans():
     )
 
     enc = OrdinalEncoder(
-        categories="auto",
+        categories=[['low', 'medium', 'high'], ['S', 'M', 'L', 'XL', 'XXL']],
         dtype=np.float64,
         handle_unknown="use_encoded_value",
         unknown_value=np.nan,
@@ -417,10 +415,9 @@ def test_podani_opt_cat_ord_with_nans():
     )
     enc.fit(data)
     data = enc.transform(data)
-    data = np.where(np.isnan(data), np.nan, data.astype(np.int32))
+    data = np.where(np.isnan(data), np.nan, data.astype(int))
 
     gower.fit(data)
-    res = gower(data[1], data[2])
-    expected_result = 1
-    tolerance = 0.0001
-    assert res-expected_result < tolerance
+    res = gower(data[2], data[4])
+    expected_result = 0
+    assert np.isclose(res, expected_result)